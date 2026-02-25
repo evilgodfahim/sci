a5a56aaec2dec65d609c471ef10b6981102aa886
Fahim <49230752+evilgodfahim@users.noreply.github.com>
2026-02-26 01:52:42 +0600

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ FEEDS = [
 "https://www.scientificamerican.com/platform/syndication/rss/",
 "https://evilgodfahim.github.io/pt/feeds/feed.xml",
 "https://feeds.feedburner.com/zmescience",
-    "https://phys.org/rss-feed/earth-news/",
     "https://feeds.newscientist.com/science-news",
     "https://www.thethirdpole.net/en/feed/",
     "https://www.carbonbrief.org/rss",
